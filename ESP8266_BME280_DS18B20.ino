e483030aa74f6dbe7bc21a6936b1b2409e33b843
Martin <martinius96@gmail.com>
2019-07-28 16:24:35 +0200

    Update ESP8266_BME280_DS18B20.ino

--- ESP8266_BME280_DS18B20.ino
+++ ESP8266_BME280_DS18B20.ino
@@ -1,3 +1,4 @@
+//DEEPSLEEP ZAVEDENY, PREPOJTE GPIO16 (D0) s RST PINOM
 #include <ESP8266WiFi.h>
 #include <DallasTemperature.h>
 #include <OneWire.h>
@@ -33,6 +34,9 @@ void setup() {
   Serial.println("");
   Serial.println("Wifi pripojene s IP:");
   Serial.println(WiFi.localIP());
+  odosli_teploty();
+  skontroluj_reset();
+  ESP.deepSleep(30e7);
 }
 
 
@@ -86,14 +90,5 @@ void skontroluj_reset() {
   }
 }
 void loop() {
-  if (WiFi.status() != WL_CONNECTED) {
-    WiFi.begin(ssid, password);
-  }
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  odosli_teploty();
-  skontroluj_reset();
-  delay(300000);
+
 }
